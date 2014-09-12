2a671de13c7b998c7677eeeeeb23b29e6b3fe5dd
Par39 <ninni_kopsa@hotmail.com>
2014-09-12 13:10:16 +0300

    Debugtulostus tiedostoon

---
+++ WriteToFile.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+void WriteToFile(std::ofstream &outputFile, std::string msg)
+{
+	outputFile << msg << std::endl;
+}
+
+int main()
+{
+	std::ofstream outputFile;
+	outputFile.open("ErrorLog.txt");
+	if (outputFile.is_open())
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			WriteToFile(outputFile, "This is a test message 3.3");
+		}
+		outputFile.close();
+	}
+	return 0;
+}
