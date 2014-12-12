c69ae6ba4b47dc25f6b634b5e0b7db0b830c83db
Par39 <ninni_kopsa@hotmail.com>
2014-12-12 13:57:57 +0200

    Menu things

--- CancerMotor/CancerGame/Game.cpp
+++ CancerMotor/CancerGame/Game.cpp
@@ -9,9 +9,44 @@ Game::Game()
 	gcontext.Initialize(&rContext);
 	glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
 
-	srand(time(NULL));
-	float mx, my;
-	Player p;
+	GameState gameState = MainMenu;
+
+	if (gameState == MainMenu)
+	{
+		Menu m;
+
+		CML::CMRectangle background = CML::CMRectangle(500, 500, 1024, 768);
+		background.SetImage(CML::ResourceManager::createResource<CML::CMImage>("MenuBackground.png"));
+		background.SetRotation(0.0f);
+		background.SetSize(CML::CMVector2<float>(-1.0f, 1.5f));
+		background.SetColor(0.0f, 0.0f, 0.0f, 0.0f);
+		background.SetOrigon(512, 384);
+
+		while (true)
+		{
+			window.WindowMessageCheck();
+			gcontext.Draw(&background);
+
+			gcontext.Draw(m.returnShape());
+
+			gcontext.EndDraw();
+
+			if (CML::CMInput::isKeyPressed(CML::CMInput::Return))
+			{
+				gameState = Play;
+				break;
+			}
+
+			if (CML::CMInput::isKeyPressed(CML::CMInput::Escape))
+				break;
+		}
+	}
+
+	if (gameState == Play)
+	{
+		srand(time(NULL));
+		float mx, my;
+		Player p;
 		CML::CMRectangle background(0, 0, 1000, 1000);
 		background.SetImage(CML::ResourceManager::createResource<CML::CMImage>("background.jpg"));
 
@@ -19,123 +54,124 @@ Game::Game()
 		tempbackground.SetImage(CML::ResourceManager::createResource<CML::CMImage>("background.jpg"));
 
 		std::vector<Projectile*>::iterator it;
-	while (true)
-	{
-		window.WindowMessageCheck();
-		gcontext.Draw(&background);
-		gcontext.Draw(&tempbackground);
-
-		gcontext.Draw(p.returnPaska(p.PLAYER));
-		gcontext.Draw(p.returnPaska(p.CURSOR));
-		if (p.GetX() < CML::CMInput::getMouseX(window.CMWindowHandle()))
+		while (true)
 		{
-			p.GetCursor()->SetWidth(75);
-			p.GetCursor()->SetHeight(-75);
-			p.SetWidth(-200);
-		}
-		else
-		{
-			p.SetWidth(200);
-			p.GetCursor()->SetWidth(-75);
-			p.GetCursor()->SetHeight(-75);
-		}
+			window.WindowMessageCheck();
+			gcontext.Draw(&background);
+			gcontext.Draw(&tempbackground);
 
-		// Scrolling background
+			gcontext.Draw(p.returnPaska(p.PLAYER));
+			gcontext.Draw(p.returnPaska(p.CURSOR));
+			if (p.GetX() < CML::CMInput::getMouseX(window.CMWindowHandle()))
+			{
+				p.GetCursor()->SetWidth(75);
+				p.GetCursor()->SetHeight(-75);
+				p.SetWidth(-200);
+			}
+			else
+			{
+				p.SetWidth(200);
+				p.GetCursor()->SetWidth(-75);
+				p.GetCursor()->SetHeight(-75);
+			}
 
-		background.SetX(background.GetX() + 10);
-		tempbackground.SetX(tempbackground.GetX() + 10);
+			// Scrolling background
 
-		if (background.GetX() > window._windowWidht )
-		{
-			background.SetX(tempbackground.GetX() - background.GetWidth());
-		}
-			
-		if (tempbackground.GetX() > window._windowWidht )
-		{
-			tempbackground.SetX(background.GetX() - tempbackground.GetWidth());
-		}
+			background.SetX(background.GetX() + 10);
+			tempbackground.SetX(tempbackground.GetX() + 10);
+
+			if (background.GetX() > window._windowWidht)
+			{
+				background.SetX(tempbackground.GetX() - background.GetWidth());
+			}
+
+			if (tempbackground.GetX() > window._windowWidht)
+			{
+				tempbackground.SetX(background.GetX() - tempbackground.GetWidth());
+			}
 
 
-		mx = CML::CMInput::getMouseX(window.CMWindowHandle());
-		my = abs((CML::CMInput::getMouseY(window.CMWindowHandle()) - (window._windowHeight)));
+			mx = CML::CMInput::getMouseX(window.CMWindowHandle());
+			my = abs((CML::CMInput::getMouseY(window.CMWindowHandle()) - (window._windowHeight)));
 
-		p.playerInputs(mx, my);
-		
+			p.playerInputs(mx, my);
 
-		if (CML::CMInput::isMouseKeyPressed(CML::CMInput::Mouse1))
-		{
-			for (int i = 0; i < 1; i++)
-			ProjectileList.push_back(new Projectile(mx+ rand() % 20, my + rand() % 20, p.GetX(), p.GetY()));
-		}
+
+			if (CML::CMInput::isMouseKeyPressed(CML::CMInput::Mouse1))
+			{
+				for (int i = 0; i < 1; i++)
+					ProjectileList.push_back(new Projectile(mx + rand() % 20, my + rand() % 20, p.GetX(), p.GetY()));
+			}
 
 			if (CML::CMInput::isMouseKeyPressed(CML::CMInput::Mouse2))
 			{
 				EnemyList.push_back(new Enemy(mx, my));
-			}	
-
-							
-					
-							for (int i = 0; i < ProjectileList.size(); i++)
-							{
-								
-								std::cout << ProjectileList[i]->GetSpeed().getY() << std::endl;
-
-							ProjectileList[i]->MoveProjectiles();
-							gcontext.Draw(ProjectileList[i]->returnPaska());
-
-								if (ProjectileList.at(i)->returnPaska()->GetX() < 0 || ProjectileList.at(i)->returnPaska()->GetX() > 1000)
-								{
-									ProjectileList.at(i)->returnPaska()->~CMShape();
-									ProjectileList.erase(ProjectileList.begin() + i);
-									continue;
-								}
-								if (ProjectileList.at(i)->returnPaska()->GetY() < 0 || ProjectileList.at(i)->returnPaska()->GetY() > 1000)
-								{
-									ProjectileList.at(i)->returnPaska()->~CMShape();
-									ProjectileList.erase(ProjectileList.begin() + i);
-								}
+			}
 
 
-								for (int k = 0; k < EnemyList.size(); k++)
-								{
-									
 
-									bool asd = CML::Collision::CollisionRectangle((EnemyList[k]->returnShape()), ProjectileList[i]->returnPaska());
-									if ( asd == true)
-									{
-										ProjectileList.at(i)->returnPaska()->~CMShape();
-										ProjectileList.erase(ProjectileList.begin() + i);
-										EnemyList.at(k)->returnShape()->~CMShape();
-										EnemyList.erase(EnemyList.begin() + k);
-										break;
-									}
-									float distance = CML::CMVector2<float>::Distance(CML::CMVector2<float>(ProjectileList[i]->returnPaska()->GetX(), ProjectileList[i]->returnPaska()->GetY()), CML::CMVector2<float>(EnemyList[k]->returnShape()->GetX(), EnemyList[k]->returnShape()->GetY()));
-									
-									if (distance < 250.0f
-										&& !EnemyList[k]->_jumping
-										&& ProjectileList[i]->GetSpeed().getY() < 5)
-									{
-										EnemyList[k]->Jump();
-									}
-									
-								}
+			for (int i = 0; i < ProjectileList.size(); i++)
+			{
 
-							}
-							
-						
-							for (int j = 0; j < EnemyList.size(); j++)
-							{
-								gcontext.Draw(EnemyList[j]->returnShape());
-								EnemyList[j]->Update(p.GetX());
+				std::cout << ProjectileList[i]->GetSpeed().getY() << std::endl;
 
-								
-							}
-						
+				ProjectileList[i]->MoveProjectiles();
+				gcontext.Draw(ProjectileList[i]->returnPaska());
 
-		gcontext.EndDraw();
+				if (ProjectileList.at(i)->returnPaska()->GetX() < 0 || ProjectileList.at(i)->returnPaska()->GetX() > 1000)
+				{
+					ProjectileList.at(i)->returnPaska()->~CMShape();
+					ProjectileList.erase(ProjectileList.begin() + i);
+					continue;
+				}
+				if (ProjectileList.at(i)->returnPaska()->GetY() < 0 || ProjectileList.at(i)->returnPaska()->GetY() > 1000)
+				{
+					ProjectileList.at(i)->returnPaska()->~CMShape();
+					ProjectileList.erase(ProjectileList.begin() + i);
+				}
 
-		if (CML::CMInput::isKeyPressed(CML::CMInput::Escape))
-			break;
+
+				for (int k = 0; k < EnemyList.size(); k++)
+				{
+
+
+					bool asd = CML::Collision::CollisionRectangle((EnemyList[k]->returnShape()), ProjectileList[i]->returnPaska());
+					if (asd == true)
+					{
+						ProjectileList.at(i)->returnPaska()->~CMShape();
+						ProjectileList.erase(ProjectileList.begin() + i);
+						EnemyList.at(k)->returnShape()->~CMShape();
+						EnemyList.erase(EnemyList.begin() + k);
+						break;
+					}
+					float distance = CML::CMVector2<float>::Distance(CML::CMVector2<float>(ProjectileList[i]->returnPaska()->GetX(), ProjectileList[i]->returnPaska()->GetY()), CML::CMVector2<float>(EnemyList[k]->returnShape()->GetX(), EnemyList[k]->returnShape()->GetY()));
+
+					if (distance < 250.0f
+						&& !EnemyList[k]->_jumping
+						&& ProjectileList[i]->GetSpeed().getY() < 5)
+					{
+						EnemyList[k]->Jump();
+					}
+
+				}
+
+			}
+
+
+			for (int j = 0; j < EnemyList.size(); j++)
+			{
+				gcontext.Draw(EnemyList[j]->returnShape());
+				EnemyList[j]->Update(p.GetX());
+
+
+			}
+
+
+			gcontext.EndDraw();
+
+			if (CML::CMInput::isKeyPressed(CML::CMInput::Escape))
+				break;
+		}
 	}
 }
 
--- CancerMotor/CancerGame/Menu.cpp
+++ CancerMotor/CancerGame/Menu.cpp
@@ -3,12 +3,12 @@
 
 Menu::Menu()
 {
-	menu = CML::CMRectangle(350, 350, 500, 500);
-	menu.SetImage(CML::ResourceManager::createResource<CML::CMImage>("noppia.png"));
+	menu = CML::CMRectangle(500, 500, 500, 500);
+	menu.SetImage(CML::ResourceManager::createResource<CML::CMImage>("Menu.png"));
 	menu.SetRotation(0.0f);
-	menu.SetSize(1.0f);
+	menu.SetSize(CML::CMVector2<float>(-1.0f, 1.0f));
 	menu.SetColor(0.0f, 0.0f, 0.0f, 0.0f);
-	menu.SetOrigon(-100, -100);
+	menu.SetOrigon(250, 250);
 }
 
 
@@ -19,4 +19,54 @@ Menu::~Menu()
 CML::CMShape *Menu::returnShape()
 {
 	return &menu;
+}
+
+float Menu::GetX()
+{
+	return menu.GetX();
+}
+
+float Menu::GetY()
+{
+	return menu.GetY();
+}
+
+void Menu::SetX(float x)
+{
+	menu.SetX(x);
+}
+
+void Menu::SetY(float y)
+{
+	menu.SetY(y);
+}
+
+float Menu::GetWidth()
+{
+	return menu.GetWidth();
+}
+
+float Menu::GetHeight()
+{
+	return menu.GetHeight();
+}
+
+void Menu::SetWidth(float x)
+{
+	menu.SetWidth(x);
+}
+
+void Menu::SetHeight(float y)
+{
+	menu.SetHeight(y);
+}
+
+void Menu::SetRotation(float a)
+{
+	menu.SetRotation(a);
+}
+
+float Menu::GetRotation()
+{
+	return menu.GetRotation();
 }
